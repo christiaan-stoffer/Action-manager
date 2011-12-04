50e3aa600f724cf87a5569b2f8eeb73c8b31d882
Christiaan Stoffer <christiaan@stofferit.nl>
2011-12-04 21:59:13 +0100

    Added wpf client and controls

---
+++ Sit.ActionsManager.Gui.Controls/ActionManager.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Sit.ActionsManager.Gui.Controls
+{
+    /// <summary>
+    /// Interaction logic for ActionManager.xaml
+    /// </summary>
+    public partial class ActionManager : UserControl
+    {
+        public ActionManager()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Sit.ActionsManager/IO/SolutionParser.cs
+++ Sit.ActionsManager/IO/SolutionParser.cs
@@ -1,6 +1,21 @@
-﻿namespace Sit.ActionsManager.IO
+﻿using System;
+
+namespace Sit.ActionsManager.IO
 {
     public class SolutionParser
     {
+        private string _solutionFilePath;
+
+        private const string XsdResourcePath = "Sit.ActionsManager.Xsd.solution.xsd";
+
+        public SolutionParser(string path)
+        {
+            if (path == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            _solutionFilePath = path;
+        }
     }
 }
